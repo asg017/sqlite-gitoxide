f7e92c8675b435d88e8690b65229ac1e8760065a
Alex Garcia <alexsebastian.garcia@gmail.com>
2025-06-02 08:10:37 -0700

    rm dbg

--- dbg.sql
+++
@@ -1,9 +0,0 @@
-.load dist/debug/git0
-.mode qbox
-
-
-select 
-  *,
-  git_at(repo, commit_id, 'README.md')
-from git_log('/Users/alex/work/simonw/datasette-alerts/examples/florida-power/scrape-florida-outages')
-limit 10;
